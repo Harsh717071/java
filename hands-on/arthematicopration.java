52ed8dc159a382e1df1a416e6e57a005070cd6af
Harsh717071 <105192704+Harsh717071@users.noreply.github.com>
2022-05-11 11:25:20 +0530

    Add files via upload

---
+++ hands-on/arthematicopration.java
@@ -0,0 +1,19 @@
+package corejava;
+
+public class arthematicopration {
+	public static void main(String[]args)
+	{
+		int a = 5 ,b = 6 ,c , d ,e , f ;
+	   
+	   c=  a + b ;
+	   d = a-b ;
+	   e = a * b ;
+	   f = a/b;
+		System.out.println("addition:" + c );
+		System.out.println("substraction:" + d);
+		System.out.println("multiply :" + e);
+		System.out.println("division : " + f);
+		
+	}
+
+}
