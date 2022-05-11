a9c6b8a88b3558a075734290845b5dda2619471b
Harsh717071 <105192704+Harsh717071@users.noreply.github.com>
2022-05-11 10:47:42 +0530

    Add files via upload

---
+++ practical/arthematicopration.java
@@ -0,0 +1,17 @@
+package corejava;
+
+public class arthematicopration {
+	public static void main(String[]args)
+	{
+		int a ,b ,c ;
+		a = 5;
+        b = 6;    
+	//c= a + b ;
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a/b);
+		
+	}
+
+}
