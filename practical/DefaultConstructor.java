977172380b545998b4c20f4fb8dd7d2755935119
Harsh717071 <105192704+Harsh717071@users.noreply.github.com>
2022-05-20 10:23:53 +0530

    Add files via upload

---
+++ practical/DefaultConstructor.java
@@ -0,0 +1,10 @@
+package corejava;
+
+ class DefaultConstructor {
+	 DefaultConstructor() {
+		 System.out.println("this is default constructor / no argument constructor");
+	 }
+   public static void main(String[]args) {
+	   DefaultConstructor c = new  DefaultConstructor();
+   }
+}
