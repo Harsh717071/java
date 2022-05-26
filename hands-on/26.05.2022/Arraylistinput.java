d7813c315a0b5daec43d2d863dc5b0e930ab0826
Harsh717071 <105192704+Harsh717071@users.noreply.github.com>
2022-05-26 13:24:30 +0530

    Update Arraylistinput.java

--- hands-on/26.05.2022/Arraylistinput.java
+++ hands-on/26.05.2022/Arraylistinput.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
-public class Arraylistinput {
+ class Arraylistinput {
  public static void main(String[]args) {
 	 Scanner sc = new Scanner(System.in);
 	 System.out.println("enter the first name");
