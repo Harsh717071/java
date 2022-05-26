79c73431e21b03811708200095f43a1af36607d8
Harsh717071 <105192704+Harsh717071@users.noreply.github.com>
2022-05-26 13:24:53 +0530

    Update Multiplication.java

--- hands-on/26.05.2022/Multiplication.java
+++ hands-on/26.05.2022/Multiplication.java
@@ -2,7 +2,7 @@ package corejava;
 
 import java.util.Scanner;
 
-public class Multiplication {
+class Multiplication {
  public static void main(String[]args) {
 	 Scanner sc= new Scanner(System.in);
 	 System.out.println("Enter the number");
